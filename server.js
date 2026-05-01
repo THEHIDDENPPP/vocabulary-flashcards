b43be6159a72f0dd34192188d5ab454fc6e25a45
PRANAV PATIL <pranavsh274@gmail.com>
2026-05-01 21:15:13 +0530

    fix vercel server setup

--- server.js
+++ server.js
@@ -136,5 +136,4 @@ app.delete('/api/words/:id', verifyToken, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
